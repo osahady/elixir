c8f467020723739484bcfbf27edb77662e805061
osahady <osahady@gmail.com>
2022-10-09 00:57:59 +0300

    documenting deal function

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -2,6 +2,19 @@ defmodule Cards do
   @moduledoc """
   Documentation for `Cards`.
   """
+
+  @doc """
+  Creating virtual cards on the deck
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+      "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+      "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+      "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+  """
   def create_deck() do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -19,6 +32,21 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+  Distribute cards by `hand_size` number and
+  the rest of the cards on remain on the deck
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.deal(deck, 3)
+      {["Ace of Spades", "Two of Spades", "Three of Spades"],
+      ["Four of Spades", "Five of Spades", "Ace of Clubs", "Two of Clubs",
+       "Three of Clubs", "Four of Clubs", "Five of Clubs", "Ace of Hearts",
+       "Two of Hearts", "Three of Hearts", "Four of Hearts", "Five of Hearts",
+       "Ace of Diamonds", "Two of Diamonds", "Three of Diamonds", "Four of Diamonds",
+       "Five of Diamonds"]}
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
