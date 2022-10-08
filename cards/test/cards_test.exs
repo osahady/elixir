70525c8a7d3ca72ca19a9faa8b1fe419c9a11da5
osahady <osahady@gmail.com>
2022-10-09 01:04:01 +0300

    writing tests: count cards are true

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,11 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "count of cards are 20" do
+    assert length(Cards.create_deck()) == 20
+  end
+
+  test "simple truth" do
+    assert 1 + 1 == 2
   end
 end
