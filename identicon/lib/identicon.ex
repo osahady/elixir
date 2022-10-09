3d0715dbf75bfc6e43b490a58862e6b8a1b86842
osahady <osahady@gmail.com>
2022-10-09 15:11:39 +0300

    added grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -7,6 +7,17 @@ defmodule Identicon do
     input
     |> hash_input()
     |> pick_color()
+    |> build_grid()
+  end
+
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+    hex_list
+    |> Enum.chunk_every(3)
+    |> List.delete_at(5)
+  end
+
+  def mirror_row(list) do
+    list = Enum.reverse(list)
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _]} = image) do
