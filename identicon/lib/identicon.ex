a56d7c991199a6bf4d4468ed1aeb40a4d82e3ccb
osahady <osahady@gmail.com>
2022-10-09 18:25:31 +0300

    removing odd hex code with filter

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -8,6 +8,17 @@ defmodule Identicon do
     |> hash_input()
     |> pick_color()
     |> build_grid()
+    |> filter_odd_squares()
+  end
+
+  # if it is odd we gonna remove the item
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter(grid, fn {num, _} = x ->
+        rem(num, 2) == 0
+      end)
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex_list} = image) do
