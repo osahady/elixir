d905cf9c013e6e0f42ecf7eff39fe1e1d09a812e
osahady <osahady@gmail.com>
2022-10-12 08:48:31 +0300

    path var -> router -> controller -> view

--- hello/lib/hello_web/controllers/hello_controller.ex
+++ hello/lib/hello_web/controllers/hello_controller.ex
@@ -5,7 +5,7 @@ defmodule HelloWeb.HelloController do
     render(conn, "index.html")
   end
 
-  def show(conn, %{"messenger" => messenger}) do
-      render(conn, "show.html", messenger: messenger)
+  def show(conn, %{"person" => val}) do
+      render(conn, "show.html", variable: val)
   end
 end
