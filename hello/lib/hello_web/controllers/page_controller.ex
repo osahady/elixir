2582a8f6cf7ac043a39d347435c199a4fb49ae5f
osahady <osahady@gmail.com>
2022-10-13 21:59:14 +0300

    sending response

--- hello/lib/hello_web/controllers/page_controller.ex
+++ hello/lib/hello_web/controllers/page_controller.ex
@@ -1,10 +1,20 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
+  alias Plug.Conn
 
   def index(conn, _params) do
-    render(conn, :index)
+    # send_resp(conn, 201, "")
+    # render(conn, :index)
     # conn
     # |> put_root_layout("admin.html")
     # |> render("index.html")
+
+    # conn
+    # |> put_resp_content_type("text/xml")
+    # |> render("index.xml", content: some_xml_content)
+
+    conn
+    |> put_status(202)
+    |> render("index.html")
   end
 end
