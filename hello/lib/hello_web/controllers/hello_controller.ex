0036085c0b73a5831480278ff5014bf80eb7a7f1
osahady <osahady@gmail.com>
2022-10-13 18:59:53 +0300

    rendering an html and passing varables

--- hello/lib/hello_web/controllers/hello_controller.ex
+++ hello/lib/hello_web/controllers/hello_controller.ex
@@ -1,11 +1,18 @@
 defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
+  alias Plug.Conn
 
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def show(conn, %{"person" => val}) do
-    render(conn, "show.html", variable: val)
+    # text(conn, "from controller #{val}")
+    # json(conn, %{name: val})
+    # render(conn, "show.html", variable: val)
+     conn
+    |> assign(:variable, val)
+    |> assign(:variable2, "THERE IS NO GOD BUT ALLAH")
+    |> render("show.html")
   end
 end
--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -21,9 +21,16 @@ defmodule HelloWeb.Router do
     get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:person", HelloController, :show
-    resources "/users", UserController
-    resources "/posts", PostController, only: [:index, :show]
-    resources "/comments", CommentController, except: [:delete]
+    resources "/users", UserController do
+      resources "/posts", PostController
+    end
+    # resources "/comments", CommentController, except: [:delete]
+  end
+
+  scope "/admin", HelloWeb.Admin do
+    pipe_through :browser
+
+    resources "/reviews", ReviewController
   end
 
   # Other scopes may use custom stacks.
