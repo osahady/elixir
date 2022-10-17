199521b41f89231c75f6b184cf31054aed5df3fb
osahady <osahady@gmail.com>
2022-10-17 09:38:52 +0300

    many to many relation product and category

--- ecommerce/lib/ecommerce/catalog.ex
+++ ecommerce/lib/ecommerce/catalog.ex
@@ -35,7 +35,9 @@ defmodule Ecommerce.Catalog do
       ** (Ecto.NoResultsError)
 
   """
-  def get_product!(id), do: Repo.get!(Product, id)
+  def get_product!(id) do
+    Product |> Repo.get(id) |> Repo.preload(:categories)
+  end
 
   @doc """
   Creates a product.
@@ -51,7 +53,7 @@ defmodule Ecommerce.Catalog do
   """
   def create_product(attrs \\ %{}) do
     %Product{}
-    |> Product.changeset(attrs)
+    |> change_product(attrs)
     |> Repo.insert()
   end
 
@@ -69,7 +71,7 @@ defmodule Ecommerce.Catalog do
   """
   def update_product(%Product{} = product, attrs) do
     product
-    |> Product.changeset(attrs)
+    |> change_product(attrs)
     |> Repo.update()
   end
 
@@ -99,7 +101,12 @@ defmodule Ecommerce.Catalog do
 
   """
   def change_product(%Product{} = product, attrs \\ %{}) do
-    Product.changeset(product, attrs)
+    categories = list_categories_by_id(attrs["category_ids"])
+    
+    product
+    |> Repo.preload(:categories)
+    |> Product.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:categories, categories)
   end
 
   def inc_page_views(%Product{} = product) do
@@ -204,5 +211,10 @@ defmodule Ecommerce.Catalog do
   """
   def change_category(%Category{} = category, attrs \\ %{}) do
     Category.changeset(category, attrs)
+  end
+
+  def list_categories_by_id(nil), do: []
+  def list_categories_by_id(category_ids) do
+    Repo.all(from c in Category, where: c.id in ^category_ids)
   end
 end
--- ecommerce/lib/ecommerce/catalog/product.ex
+++ ecommerce/lib/ecommerce/catalog/product.ex
@@ -1,12 +1,15 @@
 defmodule Ecommerce.Catalog.Product do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Ecommerce.Catalog.Category
 
   schema "products" do
     field :description, :string
     field :price, :decimal
     field :title, :string
     field :views, :integer
+
+    many_to_many :categories, Category, join_through: "product_categories", on_replace: :delete
 
     timestamps()
   end
