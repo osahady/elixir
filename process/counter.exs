f097e5dcef72e1e93ba5f230f142c5159f075fb0
osahady <osahady@gmail.com>
2022-10-30 17:10:34 +0300

    counter with receive macro

---
+++ process/counter.exs
@@ -0,0 +1,17 @@
+defmodule Counter do
+  def run(count) do
+    receive do
+      :increment ->
+        count + 1
+
+      :decrement ->
+        count - 1
+
+      msg ->
+        IO.inspect(msg, label: "Unknown Msg")
+        count
+    end
+    |> IO.inspect(label: "Count")
+    |> run()
+  end
+end
