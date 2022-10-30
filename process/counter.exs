f080f4905e57f9c09f8edcc4720f4bb3e7b8a98b
osahady <osahady@gmail.com>
2022-10-30 19:12:41 +0300

    improved counter with send and receive

--- process/counter.exs
+++ process/counter.exs
@@ -1,17 +1,45 @@
 defmodule Counter do
-  def run(count) do
+  def start_link(intial \\ 0) when is_integer(intial) do
+    spawn_link(Counter, :run, [intial])
+  end
+
+  def increment(pid) when is_pid(pid) do
+    send(pid, :increment)
+  end
+
+  def decrement(pid) when is_pid(pid) do
+    send(pid, :decrement)
+  end
+
+  def count(pid) when is_pid(pid) do
+    send(pid, {:count, self()})
+
     receive do
-      :increment ->
-        count + 1
-
-      :decrement ->
-        count - 1
-
-      msg ->
-        IO.inspect(msg, label: "Unknown Msg")
+      {^pid, count} ->
         count
     end
-    |> IO.inspect(label: "Count")
+  end
+
+  # --------new process call--------------
+  def run(count) do
+    receive do
+      msg ->
+        handle(msg, count)
+    end
+    # |> IO.inspect(label: "Count")
     |> run()
+  end
+
+  def handle(:increment, count), do: count + 1
+  def handle(:decrement, count), do: count - 1
+
+  def handle({:count, callerPid}, count) do
+    send(callerPid, {self(), count})
+    count
+  end
+
+  def handle(msg, count) do
+    IO.inspect(msg, label: "Unknown Msg")
+    count
   end
 end
