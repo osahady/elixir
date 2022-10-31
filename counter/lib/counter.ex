d35c5d1e643ac2f841b6513c6a5f732c00644f43
osahady <osahady@gmail.com>
2022-10-31 17:33:37 +0300

    rewriting some client functions: idomatic GenServer

--- counter/lib/counter.ex
+++ counter/lib/counter.ex
@@ -1,10 +1,33 @@
 defmodule Counter do
+  use GenServer
+
+  # client
+  def start_link(intial_count) do
+    GenServer.start_link(__MODULE__, intial_count)
+  end
+
+  def increment(pid) do
+    GenServer.call(pid, :inc)
+  end
+
+  def decrement(pid) do
+    GenServer.call(pid, :dec)
+  end
+
+  def current(pid) do
+    GenServer.call(pid, :current)
+  end
+
+  def divide(pid, divisor) do
+    GenServer.call(pid, {:divide, divisor})
+  end
+
+  # server
   def init(intial_count) do
     {:ok, intial_count}
   end
 
   def handle_call(:inc, _from, count) do
-    # IO.puts("from: #{inspect(from)}") from represents iex
     updated_count = count + 1
     {:reply, updated_count, updated_count}
   end
@@ -16,5 +39,9 @@ defmodule Counter do
 
   def handle_call(:current, _from, count) do
     {:reply, count, count}
+  end
+
+  def handle_call({:divide, divisor}, _from, count) do
+    {:reply, div(count, divisor), count}
   end
 end
