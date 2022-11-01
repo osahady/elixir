9a424378649473a623e1481b9bfadca4cd5f8c18
osahady <osahady@gmail.com>
2022-11-01 14:03:05 +0300

    poller project data

---
+++ know_then/exit_polling/poller/lib/poller/question.ex
@@ -0,0 +1,12 @@
+defmodule Poller.Question do
+  alias __MODULE__
+
+  defstruct id: nil, description: nil, choices: []
+
+  def new(id, description) do
+    %Question{
+      id: id,
+      description: description
+    }
+  end
+end
