79fd79f1db796dfa64e42c606f236d261945cfb6
osahady <osahady@gmail.com>
2022-11-03 16:02:20 +0300

    questions migration, schema and functions

--- know_then/exit_polling/poller_dal/lib/poller_dal/districts.ex
+++ know_then/exit_polling/poller_dal/lib/poller_dal/districts.ex
@@ -2,19 +2,20 @@ defmodule PollerDal.Districts do
   alias PollerDal.Repo
   alias PollerDal.Districts.District
 
-  def create(%District{}, attrs) do
+  # Database API
+  def create_district(attrs) do
     %District{}
     |> District.changeset(attrs)
     |> Repo.insert()
   end
 
-  def update(%District{} = district, attrs) do
+  def update_district(%District{} = district, attrs) do
     district
     |> District.changeset(attrs)
     |> Repo.update()
   end
 
-  def delete(%District{} = district) do
+  def delete_district(%District{} = district) do
     Repo.delete(district)
   end
 
---
+++ know_then/exit_polling/poller_dal/lib/poller_dal/questions/question.ex
@@ -0,0 +1,20 @@
+defmodule PollerDal.Questions.Question do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias PollerDal.Districts.District
+
+  schema "questions" do
+    field(:description, :string)
+    belongs_to(:district, District)
+
+    timestamps()
+  end
+
+  def changeset(question, attrs) do
+    question
+    |> cast(attrs, [:description, :district_id])
+    |> validate_required([:description, :district_id])
+    # checks :district_id exists in :districts table
+    |> assoc_constraint(:district)
+  end
+end
