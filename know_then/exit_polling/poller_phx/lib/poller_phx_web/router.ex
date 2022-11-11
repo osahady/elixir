c5b3a4e25975b93da684460e1bed7230e520425a
osahady <osahady@gmail.com>
2022-11-11 15:21:34 +0300

    json api for districts

--- know_then/exit_polling/poller_phx/lib/poller_phx_web/router.ex
+++ know_then/exit_polling/poller_phx/lib/poller_phx_web/router.ex
@@ -38,9 +38,11 @@ defmodule PollerPhxWeb.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", PollerPhxWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", PollerPhxWeb.Api do
+    pipe_through :api
+
+    get "/districts", DistrictController, :index
+  end
 
   # Enables LiveDashboard only for development
   #
