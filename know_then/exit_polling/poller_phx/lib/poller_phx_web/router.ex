15844b32ab8d444da230be45023da2e1bb9be66a
osahady <osahady@gmail.com>
2022-11-16 13:19:01 +0300

    integrating react with phoenix

--- know_then/exit_polling/poller_phx/lib/poller_phx_web/router.ex
+++ know_then/exit_polling/poller_phx/lib/poller_phx_web/router.ex
@@ -55,6 +55,11 @@ defmodule PollerPhxWeb.Router do
     put "/districts/:district_id/choices/:choice_id", ChoiceController, :vote
   end
 
+  scope "/app", PollerPhxWeb do
+    pipe_through :browser
+    get "/*path", AppController, :index
+  end
+
   # Enables LiveDashboard only for development
   #
   # If you want to use the LiveDashboard in production, you should put
